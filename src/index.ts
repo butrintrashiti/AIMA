c8075933638563ada097a10e772b0a38822c59c2
Butrint Rashiti <rashiti.butrint@gmail.com>
2024-01-30 22:05:34 +0100

    Initialize DB on index

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 import { Container } from 'typedi';
 import { useContainer, createExpressServer } from 'routing-controllers';
 import { ProductsController } from './controllers/products.controller';
+import { AppDataSource } from './data-source';
 
 dotenv.config();
+
+AppDataSource.initialize()
+    .then(() => {
+    })
+    .catch((error) => console.log(error))
+
 useContainer(Container);
 const port = process.env.PORT || 3000;
-
 
 const app = createExpressServer({
   controllers: [ProductsController],
